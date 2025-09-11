af323c1293a69a429b392c238a5fb6a756a709ad
Raj Kumar Karn <132379680+RAJKUMARKARN@users.noreply.github.com>
2025-09-11 15:09:52 +0530

    update..

--- src/models/Advertiser.js
+++ src/models/Advertiser.js
@@ -1,10 +1,15 @@
 const mongoose = require("mongoose");
 
 const advertiserSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  FirstName: { type: String, required: true },
+  LastName: { type: String, required: true },
+  FullName: { type: String, required: true },
   email: { type: String, required: true, unique: true },
+  age: {type:Number , required:true},
+  Education:{Education:String, required:true},
   company: { type: String },
   createdAt: { type: Date, default: Date.now },
+  MessageInput:{type:String, required:true},
 });
 
 module.exports = mongoose.model("Advertiser", advertiserSchema);
